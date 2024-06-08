a0d15f4d5bb74abf3b7761a6c744a4070d89f944
Kazeem Olanipekun <kezyolanipekun@gmail.com>
2024-06-08 16:12:45 +0100

    updated

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,6 @@ NUM_INSTANCES=${WEB_CONCURRENCY:-3}
 run_cli_tool() {
     INSTANCE_ID=$1
     python3 src/entrypoint.py create-load-balancer --nginx-port=$((PORT + INSTANCE_ID)) --heroku-api-key=$HEROKU_API_KEY --pipeline-identifier=$PIPELINE_IDENTIFIER
-    mv nginx.conf /etc/nginx/nginx.conf
 }
 
 # Export the function so GNU Parallel can use it
@@ -16,8 +15,8 @@ export -f run_cli_tool
 # Run the function in parallel instances
 parallel -j $NUM_INSTANCES run_cli_tool ::: $(seq 0 $((NUM_INSTANCES - 1)))
 
-# Concatenate all Nginx configuration files into one
-# cat /etc/nginx/nginx-*.conf > /etc/nginx/nginx.conf
+# Move  Nginx configuration files into one
+ mv nginx.conf /etc/nginx/nginx.conf
 
 # Start Nginx in the foreground
 nginx -g 'daemon off;'
