29f7e55550e8973c99166ff3d6fd2e8ce77d7277
Kazeem Olanipekun <kezyolanipekun@gmail.com>
2026-07-15 22:52:34 +0100

    updated

--- src/nginx.py
+++ src/nginx.py
@@ -1,6 +1,3 @@
-"""
-Provide implements of the Nginx domain.
-"""
 from src.constants import NGINX_LOAD_BALANCER_CONFIG_TEMPLATE
 
 
@@ -20,34 +17,56 @@ class CreationNginxLoadBalancerConfigFile:
         """
         Remove protocol and last slash from the URL.
         """
-        return url.replace('https://', '').replace('/', '')
+        return url.replace("https://", "").rstrip("/")
 
-    def with_urls(self, urls):
+    def with_urls(self, urls=None):
         """
-        Creation of Nginx load balancer config file with specified URLs.
+        Create an Nginx configuration that proxies all traffic to
+        https://api.getvent.io.
         """
-        upstream_server_localhosts_text = ''
-        upstream_server_configs_text = ''
 
-        for index, url in enumerate(urls):
-            url_without_last_slash = url[:-1]
-            host_from_url = self.get_host_from_url(url=url)
+        upstream_server_localhosts_text = "\t\tserver 127.0.0.1:8000;\n"
 
-            upstream_server_configs_text += \
-                f'\tserver <\n\t\tlisten 800{index};\n\n\t\tlocation / ' + \
-                f'<\n\t\t\tproxy_set_header HOST {host_from_url};\n\t\t\t' + \
-                f'proxy_pass {url_without_last_slash};\n\t\t>\n\t>\n\n'
+        upstream_server_configs_text = """
+\tserver <
+\t\tlisten 8000;
 
-            upstream_server_localhosts_text += f'\t\tserver 127.0.0.1:800{index};\n'
+\t\tlocation / <
+\t\t\tproxy_pass https://api.getvent.io;
 
-        nginx_load_balancer_config_file_ready_to_use = NGINX_LOAD_BALANCER_CONFIG_TEMPLATE.format(
-            port=self.port,
-            upstream_server_localhosts=upstream_server_localhosts_text,
-            upstream_server_configs=upstream_server_configs_text,
+\t\t\tproxy_http_version 1.1;
+
+\t\t\tproxy_ssl_server_name on;
+\t\t\tproxy_ssl_name api.getvent.io;
+
+\t\t\tproxy_set_header Host api.getvent.io;
+\t\t\tproxy_set_header X-Real-IP $remote_addr;
+\t\t\tproxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+\t\t\tproxy_set_header X-Forwarded-Proto $scheme;
+\t\t\tproxy_set_header X-Forwarded-Host $host;
+\t\t\tproxy_set_header X-Forwarded-Port $server_port;
+
+\t\t\tproxy_set_header Upgrade $http_upgrade;
+\t\t\tproxy_set_header Connection "upgrade";
+
+\t\t\tproxy_buffering off;
+\t\t>
+\t>
+"""
+
+        nginx_load_balancer_config_file_ready_to_use = (
+            NGINX_LOAD_BALANCER_CONFIG_TEMPLATE.format(
+                port=self.port,
+                upstream_server_localhosts=upstream_server_localhosts_text,
+                upstream_server_configs=upstream_server_configs_text,
+            )
         )
 
-        nginx_load_balancer_config_file_ready_to_use = \
-            nginx_load_balancer_config_file_ready_to_use.replace('<', '{').replace('>', '}')
+        nginx_load_balancer_config_file_ready_to_use = (
+            nginx_load_balancer_config_file_ready_to_use
+            .replace("<", "{")
+            .replace(">", "}")
+        )
 
-        with open('nginx.conf', 'w') as nginx_config:
+        with open("nginx.conf", "w") as nginx_config:
             nginx_config.write(nginx_load_balancer_config_file_ready_to_use)
